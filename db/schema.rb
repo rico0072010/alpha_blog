9715d33f8176262e2a4285a12fbdbacd2a845df4
rico0072010 <danildolin96@gmail.com>
2020-02-05 20:58:37 +0800

    adding association and style article data

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_05_104953) do
+ActiveRecord::Schema.define(version: 2020_02_05_121627) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
