2bad4c9f7da9f24afb772def71a48fc2ae8aefec
rico0072010 <danildolin96@gmail.com>
2020-02-06 20:54:24 +0800

    added user creation

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :articles
   get 'about', to: 'pages#about'
   root 'pages#homepage'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
