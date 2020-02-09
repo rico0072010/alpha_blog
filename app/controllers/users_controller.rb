44c795d7467c5e0cd78d0f472ec712d3919e92d1
rico0072010 <danildolin96@gmail.com>
2020-02-09 11:32:58 +0800

    Create show action and added Gravatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
   def new
     @user = User.new
   end
@@ -11,6 +16,20 @@ class UsersController < ApplicationController
       else
         render 'new'
       end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update
+      flash[:success] = "Account successfully updated"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
   end
 
   private
